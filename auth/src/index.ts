1e27272854f167e80846abc2cb525fe98e475ef9
stevenmcsorley <smcsorl@gmail.com>
2022-04-24 22:41:04 +0100

    connect to mongo db service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -17,13 +18,22 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', async () =>{
+app.all('*', async () => {
   throw new NotFoundError()
 });
 
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log("connected to mongo-db");
+  } catch (err) {
+    console.log(err)
+  }
+  app.listen(3000, () => {
+    console.log('listening on 3000 !!')
+  })
+}
 
-app.listen(3000, () => {
-  console.log('listening on 3000 !')
-})
+start();
