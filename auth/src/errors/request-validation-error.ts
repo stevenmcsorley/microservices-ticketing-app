f32ae55e3fd8c5d83c984baa2a8513cf2f42b2f1
stevenmcsorley <smcsorl@gmail.com>
2022-04-24 16:03:33 +0100

    add middleware for error handling

---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,12 @@
+import { ValidationError } from "express-validator";
+
+
+export class RequestValidationError extends Error {
+
+  constructor(public errors: ValidationError[]) {
+    super();
+
+    // Onl because we are extending a built in class
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+}
