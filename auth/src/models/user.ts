19b39dcd0da23f66e2269fd92d9628c680aae453
stevenmcsorley <smcsorl@gmail.com>
2022-04-25 15:32:56 +0100

    reformat json props from mongodb

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -24,10 +24,20 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
-});
+},
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      }
+    }
+  });
 
-userSchema.pre('save', async function(done){
-  if(this.isModified('password')){
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
     const hashed = await Password.toHash(this.get('password'));
     this.set('password', hashed);
   }
